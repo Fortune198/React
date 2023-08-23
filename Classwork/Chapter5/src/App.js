26d0504588b8474be75559ea5595fbeeb19d030c
Fortune198 <matshika1998@outlook.com>
2023-08-23 18:08:54 +0200

    first commit

---
+++ Classwork/Chapter5/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          My Second React App!
+        </h1>
+      </div>
+    );
+  }
+}
+export default App;
+
+// import React, { Component } from 'react';
+// import Products from './Products';
+
+// class App extends Component {
+//   render() {        
+//     return (
+//       <div>
+//         <h1>My First React App</h1>
+//         <Products />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
