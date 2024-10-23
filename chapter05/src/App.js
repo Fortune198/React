c8bbaffd0f48847f5f7e519271eb96a577824d66
Fortune198 <fortunematshika@gmail.com>
2024-10-23 12:37:54 +0200

    Update

---
+++ chapter05/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Products from './Products';
+import JumboTronComponent from './JumboTronComponent';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Products /> */}
+      <JumboTronComponent >This is a simple hero unit, a simple jumbotron-style component for
+        calling extra attention to featured content or information.
+      </JumboTronComponent>
+    </div>
+  );
+}
+
+export default App;
