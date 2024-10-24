3f2cb48e2b95d200346ce006ec1e4ac634b90f32
Fortune198 <fortunematshika@gmail.com>
2024-10-24 12:07:37 +0200

    Updated

---
+++ chapter07/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import UserForm from './UserForm';
+import GitHub from './GitHub';
+
+function App() {
+  return (
+    <div className="App">
+     <GitHub />
+    </div>
+  );
+}
+
+export default App;
